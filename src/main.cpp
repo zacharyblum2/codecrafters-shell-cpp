0f52bde643006dd37a20ac475753b3a53aaf8b1f
Zachary Blum <blum.zachary1@gmail.com>
2025-01-07 22:19:53 -0500

    Uncomment main block.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,7 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
+  std::cout << "$ ";
 
   std::string input;
   std::getline(std::cin, input);
