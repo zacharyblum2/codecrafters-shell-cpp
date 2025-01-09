630e175042207e34dedcd64b5a17d09bc429c3e8
Zachary Blum <blum.zachary1@gmail.com>
2025-01-08 21:42:36 -0500

    Attempt type builtins. Refactor for scaling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,70 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <unordered_set>
 
 using namespace std;
 
-string exitCommand = "exit 0";
-string echoCommand = "echo ";
+// List of commands
+struct Commands {
+    static constexpr const char* echo = "echo";
+    static constexpr const char* exit = "exit";
+    static constexpr const char* type = "type";
+};
+
+struct CommandPrefixes {
+  static constexpr const char* echo = "echo ";
+  static constexpr const char* exit = "exit 0";
+  static constexpr const char* type = "type ";
+};
+
+unordered_set<string> CommandsSet {
+  "echo",
+  "exit",
+  "type",
+};
+
+void handleInput(string input) {
+  // Get first word
+  string cmd;
+
+  stringstream ss(input);
+  ss >> cmd;
+
+  // C++ doesn't support string switch statements, use if else
+  if (cmd == Commands::echo) {
+    string echo = input.substr(strlen(CommandPrefixes::echo));
+    cout << echo << endl;
+  }
+  else if (cmd == Commands::type) {
+    string type = input.substr(strlen(CommandPrefixes::type));
+
+    // Verify type exists
+    if (CommandsSet.find(type) != CommandsSet.end()) {
+      cout << type << " is a shell builtin" << endl;
+    }
+    else {
+      cout << type << ": not found" << endl;
+    }
+  }
+  else {
+    cout << input << ": command not found" << endl;
+  }
+
+  // Else, print invalid command
+  cout << "$ ";
+} 
 
 int main() {
   cout << "$ ";
   string input;
   
   // REPL (Read-Evaluate-Print-Loop)
-  while (getline(cin, input) && input.find(exitCommand) != 0) {
+  while (getline(cin, input) && input.find(CommandPrefixes::exit) != 0) {
     // Flush after every std::cout / std:cerr
     cout << std::unitbuf;
     cerr << std::unitbuf;
 
-    // TODO: Create helper function for builtins
-    if (input.find(echoCommand) == 0) {
-      string echo = input.substr(echoCommand.length());
-      cout << echo << endl;
-    } else {
-      cout << input << ": command not found" << endl;
-    }
-
-    cout << "$ ";
+    handleInput(input);
   }
 }
